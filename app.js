a7d0273bf83f2839aba8b7d55a53244bc39d7ae6
Parthiv Shah <pashah@cygnet.com>
2018-07-17 18:48:35 +0530

    Setup start script and heroku port

---
+++ app.js
@@ -0,0 +1,27 @@
+var express =require('express');
+var hbs = require('hbs');
+var port =process.env.PORT || 3000 ;
+var app = express();
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+app.use(express.static('D:/node-web-server'));
+app.get('/',(req,res)=>{
+    res.send({
+        page:'new page',
+        name : 'parthiv'
+});
+});
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pagetitle : 'pageTitle',
+       // year : new Date().getFullYear()
+    });
+});
+app.listen(port,()=>{
+    console.log('server started on' +port);
+});
